fe36e1c2117e396bb7150c30a548157ada55e5ed
llyman707 <llyman707@gmail.com>
2025-09-29 06:28:31 -0400

    restructuring routes

--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,14 @@ const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin)
-
-router.post('/login',
-    storeReturnTo,
-    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
-    
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo,
+        passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout)
 
