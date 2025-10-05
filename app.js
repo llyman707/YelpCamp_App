66c27dcd0702155526ca38b37f9d98657eb25870
llyman707 <llyman707@gmail.com>
2025-10-05 18:09:49 -0400

    updating mongodb atlas credentials

--- app.js
+++ app.js
@@ -23,11 +23,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
